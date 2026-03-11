dec2b7cec654c696cc1b56917d2c91b570470cb7
Kuang-Chen (KC) Lu <klu@intersystems.com>
2026-03-11 16:29:25 -0400

    clean

--- src/langspec/index.ts
+++ src/langspec/index.ts
@@ -98,16 +98,6 @@ export function seqDrop13<T>(p1: string, p2: Parser<T>, p3: string): Parser<T> {
     return drop13(seq(str(p1), p2, str(p3)));
 }
 
-export function seqDrop15<T1, T2, T3, T4, T5>(
-    p1: Parser<T1>,
-    p2: Parser<T2>,
-    p3: Parser<T3>,
-    p4: Parser<T4>,
-    p5: Parser<T5>,
-): Parser<[T2, T3, T4]> {
-    return drop15(seq(p1, p2, p3, p4, p5));
-}
-
 export const dbg = <T>(p: Parser<T>, where = 'DBG') =>
     p.map((x) => {
         console.log(`${where}: ` + JSON.stringify(x));
