0e940ddde02ac98f76fda28f9e5aae3dd79f9f25
Kuang-Chen (KC) Lu <klu@intersystems.com>
2026-03-12 12:02:44 -0400

    lint

--- src/langspec/alt.ts
+++ src/langspec/alt.ts
@@ -1,7 +1,6 @@
 import { fail, type Parsers, succ } from './core.js';
 import { type Parser } from './core.js';
 
-
 export function alt<T extends any[]>(...ps: Parsers<T>): Parser<T[number]> {
     return ps.reduceRight((acc, p) => p.alt2(acc), fail);
 }
